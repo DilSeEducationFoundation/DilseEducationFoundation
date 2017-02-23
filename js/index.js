b95fc41e5af250b17dcaf440913a15b1f64afdef
naveenpallayil <naveennairp@gmail.com>
2017-02-23 14:45:01 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -104,6 +104,33 @@ renderImages2 = function () {
     });
 
 },
+renderImages3 = function () {
+    $.get('img/ImageList.txt', function (data) {
+        var lines = data.split('\n');
+        for (var iCount = 31; iCount <lines.length-1; iCount++) {
+            var item = 'img/DilSePhotoshoot/' + lines[iCount];
+            var portfolioGallery = '<div class="col-lg-2 col-sm-3">'
+                + '<a href="{0}" class="portfolio-box">'
+                   + '<img src="{1}" class="img-responsive" alt="">'
+                   + '<div class="portfolio-box-caption">'
+                     + '<div class="portfolio-box-caption-content">'
+                           + '<div class="project-category text-faded">'
+                           + '</div>'
+                           + ' <div class="project-name">'
+                           + '</div>'
+                        + '</div>'
+                   + '</div>'
+               + ' </a>'
+            + '</div>';
+            $("#dilseGallery3").append(portfolioGallery.replace("{0}", item).replace("{1}", item));
+        }
+        $.getScript('//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js', function () {
+            $('#portfolio .img-responsive').lazyload({
+            });
+        });
+    });
+
+},
 
 configureRenderMoreText = function(){
     var showChar = 300;
