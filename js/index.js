dee9ffddcea2ae822040801b83ccfdf66c0ac3c6
naveenpallayil <naveennairp@gmail.com>
2017-02-23 14:35:38 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -54,7 +54,7 @@ renderImages1 = function () {
         var lines = data.split('\n');
         for (var iCount = 0; iCount <lines.length-142; iCount++) {
             var item = 'img/DilSePhotoshoot/' + lines[iCount];
-            var portfolioGallery = '<div class="col-lg-2 col-sm-6 pull-left">'
+            var portfolioGallery = '<div class="col-sm-4 pull-left">'
                 + '<a href="{0}" class="portfolio-box">'
                    + '<img src="{1}" class="img-responsive" alt="">'
                    + '<div class="portfolio-box-caption">'
@@ -68,6 +68,23 @@ renderImages1 = function () {
                + ' </a>'
             + '</div>';
             $("#dilseGallery1").append(portfolioGallery.replace("{0}", item).replace("{1}", item));
+        }
+         for (var iCount = 31; iCount <lines.length-1; iCount++) {
+            var item = 'img/DilSePhotoshoot/' + lines[iCount];
+            var portfolioGallery = '<div class="col-lg-2 col-sm-3">'
+                + '<a href="{0}" class="portfolio-box">'
+                   + '<img src="{1}" class="img-responsive" alt="">'
+                   + '<div class="portfolio-box-caption">'
+                     + '<div class="portfolio-box-caption-content">'
+                           + '<div class="project-category text-faded">'
+                           + '</div>'
+                           + ' <div class="project-name">'
+                           + '</div>'
+                        + '</div>'
+                   + '</div>'
+               + ' </a>'
+            + '</div>';
+            $("#dilseGallery2").append(portfolioGallery.replace("{0}", item).replace("{1}", item));
         }
         $.getScript('//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js', function () {
             $('#portfolio .img-responsive').lazyload({
