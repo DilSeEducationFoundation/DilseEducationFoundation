09e766b1c0c6bc882a827e6444775718931c8c1b
naveenpallayil <naveennairp@gmail.com>
2017-02-23 18:54:31 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -123,7 +123,7 @@ renderImages_Events1 = function () {
         var lines = data.split('\n');
         for (var iCount = 150; iCount <lines.length-1; iCount++) {
             var item = 'img/DilSePhotoshoot/' + lines[iCount];
-            var portfolioGallery = '<div class="col-lg-2 col-sm-6">'
+            var portfolioGallery = '<div class="col-sm-4">'
                 + '<a href="{0}" class="portfolio-box">'
                    + '<img src="{1}" class="img-responsive" alt="">'
                    + '<div class="portfolio-box-caption">'
@@ -152,7 +152,7 @@ renderImages_Events2 = function () {
         var lines = data.split('\n');
         for (var iCount = 150; iCount <lines.length-1; iCount++) {
             var item = 'img/DilSePhotoshoot/' + lines[iCount];
-            var portfolioGallery = '<div class="col-lg-2 col-sm-6">'
+            var portfolioGallery = '<div class="col-sm-4">'
                 + '<a href="{0}" class="portfolio-box">'
                    + '<img src="{1}" class="img-responsive" alt="">'
                    + '<div class="portfolio-box-caption">'
@@ -181,7 +181,7 @@ renderImages_Events3 = function () {
         var lines = data.split('\n');
         for (var iCount = 150; iCount <lines.length-1; iCount++) {
             var item = 'img/DilSePhotoshoot/' + lines[iCount];
-            var portfolioGallery = '<div class="col-lg-2 col-sm-6">'
+            var portfolioGallery = '<div class="col-sm-4">'
                 + '<a href="{0}" class="portfolio-box">'
                    + '<img src="{1}" class="img-responsive" alt="">'
                    + '<div class="portfolio-box-caption">'
