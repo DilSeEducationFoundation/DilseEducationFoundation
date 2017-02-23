59a6170821a99cb71cc282edb54e562915cb6fa9
naveenpallayil <naveennairp@gmail.com>
2017-02-23 13:58:42 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     renderImages();
     configureRenderMoreText();
     renderImages1();
+    renderImages2();
 });
 var sendMail = function () {
     var contributionType = [];
@@ -67,6 +68,34 @@ renderImages1 = function () {
                + ' </a>'
             + '</div>';
             $("#dilseGallery1").append(portfolioGallery.replace("{0}", item).replace("{1}", item));
+        }
+        $.getScript('//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js', function () {
+            $('#portfolio .img-responsive').lazyload({
+            });
+        });
+    });
+
+},
+
+renderImages2 = function () {
+    $.get('img/ImageList.txt', function (data) {
+        var lines = data.split('\n');
+        for (var iCount = 0; iCount <lines.length-1; iCount++) {
+            var item = 'img/DilSePhotoshoot/' + lines[iCount];
+            var portfolioGallery = '<div class="col-lg-2 col-sm-3">'
+                + '<a href="{0}" class="portfolio-box">'
+                   + '<img src="{1}" class="img-responsive" alt="">'
+                   + '<div class="portfolio-box-caption">'
+                     + '<div class="portfolio-box-caption-content">'
+                           + '<div class="project-category text-faded">'
+                           + '</div>'
+                           + ' <div class="project-name">'
+                           + '</div>'
+                        + '</div>'
+                   + '</div>'
+               + ' </a>'
+            + '</div>';
+            $("#dilseGallery2").append(portfolioGallery.replace("{0}", item).replace("{1}", item));
         }
         $.getScript('//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js', function () {
             $('#portfolio .img-responsive').lazyload({
