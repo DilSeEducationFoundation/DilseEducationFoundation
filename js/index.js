0fc9a79e119bd44eb02b96e833b81dfe9a73f32f
naveenpallayil <naveennairp@gmail.com>
2017-02-24 15:44:15 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -48,8 +48,10 @@ renderImages_Contribution = function (columnNumber) {
             max = col3;
         }
        
-        var carouselHTMLChunk = '<div id="myCarousel'+columnNumber+'"class="carousel slide" data-ride="carousel">'+
-                                '<div id=innerCarousel'+columnNumber+'"class="carousel-inner" role="listbox"></div></div>';
+        var carouselHTMLChunk = '<div id="myCarousel" class="carousel slide" data-ride="carousel">'+
+                                '<div id=innerCarousel" class="carousel-inner" role="listbox"></div></div>';
+        
+         $("#dilseGallery"+columnNumber).append(carouselHTMLChunk);
         
         for (var iCount = 0; iCount <max; iCount++) {
             var item = 'img/DilSePhotoshoot/' + lines[iCount];
@@ -77,7 +79,7 @@ renderImages_Contribution = function (columnNumber) {
            if(limit === 9){
                 limit = 0;
                 slideHTML = slideHTML + '</div>';
-                $("#innerCarousel"+columnNumber).append(slideHTML);
+                $("#innerCarousel").append(slideHTML);
                 slideHTML = '<div class="galleryitem">';
                 
             }
@@ -86,11 +88,11 @@ renderImages_Contribution = function (columnNumber) {
             slideHTML = slideHTML + portfolioGallery.replace("{0}", item).replace("{1}", item);  
             if(iCount === max-1){
                 slideHTML = slideHTML + '</div>';
-                $("#innerCarousel"+columnNumber).append(slideHTML);
+                $("#innerCarousel").append(slideHTML);
             }           
         }
         
-         $("#dilseGallery"+columnNumber).append(slideHTML);
+        
         
         $.getScript('//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js', function () {
             $('#portfolio .img-responsive').lazyload({
